99b748acc9b56eaf8f1b4c412889e2e0aa730aff
guts-b-gif <devillers0017@gmail.com>
2024-07-28 20:21:32 +0545

    change made in admin_menu.php
    check these changes

---
+++ layouts/admin_menu.php
@@ -0,0 +1,94 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle"> 
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categories</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage products</a> </li>
+       <li><a href="add_product.php">Add product</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Sales</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Manage Sales</a> </li>
+         <li><a href="add_sale.php">Add Sale</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Sales Report</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Sales by dates </a></li>
+        <li><a href="monthly_sales.php">Monthly sales</a></li>
+        <li><a href="daily_sales.php">Daily sales</a> </li>
+      </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-sort-by-order"></i>
+       <span>Order Management</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="orders.php">Manage Orders</a></li>
+        <li><a href="add_order.php">Add Order</a></li>
+        <li><a href="order_status.php">Order Status</a></li>
+      </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-tags"></i>
+       <span>Inventory Management</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="inventory.php">Manage Inventory</a></li>
+        <li><a href="add_inventory.php">Add Inventory</a></li>
+        <li><a href="inventory_report.php">Inventory Report</a></li>
+      </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-briefcase"></i>
+       <span>Supplier Management</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="suppliers.php">Manage Suppliers</a></li>
+        <li><a href="add_supplier.php">Add Supplier</a></li>
+        <li><a href="supplier_report.php">Supplier Report</a></li>
+      </ul>
+  </li>
+</ul>
--- login_v2.php
+++ login_v2.php
@@ -3,15 +3,14 @@
   require_once('includes/load.php');
   if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
 ?>
-<?php include_once('layouts/header.php'); ?>
-<link rel="stylesheet" type="text/css" href="libs/css/index.css">
+
 <div class="login-page">
     <div class="text-center">
-       <h2>Inventory Management System</h2>
-       <h4>Sign in Here</h4>
+       <h1>Welcome</h1>
+       <p>Sign in to start your session</p>
      </div>
      <?php echo display_msg($msg); ?>
-      <form method="post" action="auth.php" class="clearfix">
+      <form method="post" action="auth_v2.php" class="clearfix">
         <div class="form-group">
               <label for="username" class="control-label">Username</label>
               <input type="name" class="form-control" name="username" placeholder="Username">
@@ -21,8 +20,8 @@
             <input type="password" name= "password" class="form-control" placeholder="password">
         </div>
         <div class="form-group">
-        <button type="submit" style="background-color: #2ecc71; color: #ffffff; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; text-align: center; margin: 0 auto; display: block;" class="btn btn-info">Login</button>
+                <button type="submit" class="btn btn-info  pull-right">Login</button>
         </div>
     </form>
 </div>
-<?php include_once('layouts/footer.php'); ?>
+<?php include_once('layouts/header.php'); ?>
